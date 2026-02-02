cb42e8cd320eb0494281805c1d8d1b6af58fa6cd
Prikey842 <markkurug120@gmail.com>
2026-02-02 07:42:57 +0000

    Lesson 4: Operators in C

---
+++ Lesson4/main.c
@@ -0,0 +1,23 @@
+// OPERATORS IN C
+#include<stdio.h>
+int main(void) {
+    int a = 43, b = 28;
+    //printf("%d\n", a + b);
+    //printf("%d\n", a / b);
+    //printf("%d\n", a % b);
+    //printf("%d\n", a > b);
+    //printf("%d\n", a < b);
+    //printf("%d\n", a != b);
+    //printf("%d\n", a == b);
+    //printf("%d\n", a > 0 && a < 10);
+    //printf("%d\n", !(a > 0));
+   // a += 3;
+    //printf("%d\n", a);
+    //printf("%d\n", ++a);
+    //int c = a++ + ++b ;
+    //printf("%d %d %d\n", a, b, c);
+    int q = 5;
+    printf("%d\n", q++);
+    printf("%d\n", q);
+    return 0;
+}
