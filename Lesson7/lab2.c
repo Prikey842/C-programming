e25b5d66028a7c99d84c9b243e346d68a582e368
Prikey842 <markkurug120@gmail.com>
2026-02-06 23:33:36 +0000

    Lesson 7: If else

---
+++ Lesson7/lab2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+    // If, if-else, else if
+    int y = 20;
+    if (y>7) {
+        printf("Positive number\n");
+    }
+    if (y>40) {
+        printf("good boy\n");
+    }
+
+
+
+    return 0;
+}
